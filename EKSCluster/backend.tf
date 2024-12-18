22cb5c3ba62c72c4fcdeed72e50e27a298255253
bkletch <bkl83@live.co.uk>
2024-12-18 20:54:48 +0000

    changed backen.tf to reference S3 bucket correctly

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "k8s-ec2-runner-bucket-bl" # CHANGE WITH YOUR S3 BUCKET
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
